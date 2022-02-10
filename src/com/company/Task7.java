763442408f3023f94e1e1da259a4705644f5703e
Diana0107K <97853647+Diana0107K@users.noreply.github.com>
2022-02-10 10:52:39 +0300

    Update Task7.java

--- src/com/company/Task7.java
+++ src/com/company/Task7.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Task7 {
     public static void main(String[] args) {
-        Pattern wordStartAndEndA = Pattern.compile("\\b[аa]((\\w*[аa]\\b)|(\\b))");
+        Pattern wordStartAndEndA = Pattern.compile("\\b[аa]+(\\b|(\\w*[аa]\\b))");
         Matcher sAe = wordStartAndEndA.matcher("aakchj lkjhb asdfc dfgchca adfgccccja.");
         System.out.println(sAe.find());
     }
